b00848505dd1676674a714ebc3cb429d8f27b09d
Ali Omran <ali.omran.24@lehre.mosbach.dhbw.de>
2025-02-05 18:09:14 +0100

    I created the deck class with the needed methods for it and also a testing method to check all functions

--- src/main/java/BluffOrBluff/model/Deck.java
+++ src/main/java/BluffOrBluff/model/Deck.java
@@ -1,4 +1,64 @@
 package BluffOrBluff.model;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class Deck {
+    private List<Card> cards;
+
+    public Deck() {
+        cards = new ArrayList<>();
+        for (Card.Suit suit : Card.Suit.values()) {
+            for (Card.Rank rank : Card.Rank.values() ){
+                cards.add( new Card(rank, suit));
+            }
+        }
+        //shuffle();
+    }
+
+    public void shuffle() {
+        Collections.shuffle(cards);
+    }
+
+    public Card dealCard() {
+        if (cards.isEmpty()) {
+            throw new IllegalStateException("No more cards to deal");
+        }
+        return cards.remove(0);
+    }
+
+    public boolean isEmpty() {
+        return cards.isEmpty();
+    }
+
+    public int cardsLeft() {
+        return cards.size();
+    }
+
+    public void printDeck() {
+        for (Card card : cards) {
+            System.out.print(card + " ");
+        }
+    }
+
+    public static void testDeck(Deck deck) {
+        deck.printDeck();
+        deck.shuffle();
+        System.out.println();
+        deck.printDeck();
+        System.out.println("\n");
+
+        while (!deck.isEmpty()) {
+            System.out.println(deck.dealCard() + "  " + deck.cardsLeft());
+        }
+        System.out.println("\nAll cards have been dealt!");
+    }
+
+    public static void main (String[] args) {
+        testDeck(new Deck());
+    }
+
 }
+
+
